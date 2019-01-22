d5cec8a7e3abafd6ac93811358d34c565ff69679
Jose Rivas <joseantonio.rivasfernandez@gmail.com>
2019-01-23 00:31:22 +0100

    Fix salt on crypto

--- nap/Core/Crypto.php
+++ nap/Core/Crypto.php
@@ -5,10 +5,17 @@ namespace Nap;
 class Crypto {
 
     protected static $cost = 12;
-    protected static $algo = PASSWORD_DEFAULT;
+    protected static $algo = PASSWORD_BCRYPT;
 
     public static function hash(string $password): string {
-        return password_hash($password, self::$algo, ['cost' => self::$cost]);
+        global $appConfig;
+        
+        $options = ['cost' => self::$cost];
+        
+        if( isset($appConfig['auth']) && isset($appConfig['auth']['salt']))
+            $options['salt'] = $appConfig['auth']['salt'];
+        
+        return password_hash($password, self::$algo, $options);
     }
 
 }
