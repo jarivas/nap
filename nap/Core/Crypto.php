c57ab8207f3dfd6a5e22cdb8e97c052ce8e5cf4c
Jose Rivas <joseantonio.rivasfernandez@gmail.com>
2019-01-23 00:04:26 +0100

    Fix on crypto

--- nap/Core/Crypto.php
+++ nap/Core/Crypto.php
@@ -8,7 +8,7 @@ class Crypto {
     protected static $algo = PASSWORD_DEFAULT;
 
     public static function hash(string $password): string {
-        return password_hash($pwd, self::$algo, ['cost' => self::$cost]);
+        return password_hash($password, self::$algo, ['cost' => self::$cost]);
     }
 
 }
