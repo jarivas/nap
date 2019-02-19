6dfcd4446dc7ed1d76be97ab23414eafa7b7e97c
Jose Rivas <joseantonio.rivasfernandez@gmail.com>
2019-02-19 14:33:04 +0100

    Fix on cors, HTTP_ORIGIN is only present on php dev server

--- nap/functions/cors.php
+++ nap/functions/cors.php
@@ -2,8 +2,8 @@
 
 use Nap\Response;
 
-function handleCors($allowedOrigin, $allowedHeaders, $allowedMethods, $method) {
-    header("Access-Control-Allow-Origin: $allowedOrigin");
+function handleCors($allowedHeaders, $allowedMethods, $method) {
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: $allowedHeaders");
     header("Access-Control-Allow-Methods: $allowedMethods");
 
@@ -14,19 +14,13 @@ function handleCors($allowedOrigin, $allowedHeaders, $allowedMethods, $method) {
 if (isset($appConfig['cors'])) {
     $cors = &$appConfig['cors'];
     $allowedHeaders = $cors['allowed-headers'];
-    $allowedOrigin = $cors['allowed-origin'];
     $allowedMethods = $cors['allowed-methods'];
-    $url = $_SERVER['HTTP_ORIGIN'];
 
     if (stripos($allowedMethods, $method) === false)
         throw new Exception('Method not allowed', Response::WARNING_TYPE_METHOD_NOT_ALLOWED);
 
-    if (($allowedOrigin == '*') || ($allowedOrigin == $url))
-        handleCors($allowedOrigin, $allowedHeaders, $allowedMethods, $method);
-    else
-        throw new Exception('CORS not enabled', Response::WARNING_TYPE_BAD_REQUEST);
-
-    unset($allowedOrigin);
+    handleCors($allowedHeaders, $allowedMethods, $method);
+    
     unset($allowedHeaders);
     unset($allowedMethods);
     unset($corsOK);
